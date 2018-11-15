be5d39007891125a6e609a05415896a4fe4147c9
Jason Kingsbury <jason@relva.co.uk>
2018-11-15 17:35:53 +0000

    ensure a wrapped reader is closed, remove migrated code

--- pipes/replace.go
+++ pipes/replace.go
@@ -1,15 +1,12 @@
 package pipes
 
 import (
-	"bufio"
-	"bytes"
 	"context"
 	"github.com/pkg/errors"
 	"github.com/relvacode/pipe"
 	"github.com/relvacode/pipe/console"
 	"github.com/relvacode/pipe/tap"
 	"github.com/relvacode/rewrite"
-	"io"
 )
 
 func init() {
@@ -23,104 +20,6 @@ func init() {
 			}
 		},
 	})
-}
-
-func NewStreamReplacer(r io.Reader, what, with []byte) *StreamReplacer {
-	return &StreamReplacer{
-		From:    bufio.NewReader(r),
-		What:    what,
-		With:    with,
-		replace: new(bytes.Buffer),
-	}
-}
-
-// StreamReplacer is a content replacing proxy for a byte stream
-type StreamReplacer struct {
-	From *bufio.Reader
-	What []byte
-	With []byte
-
-	doReplace bool
-	replace   *bytes.Buffer
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func (sr *StreamReplacer) Read(p []byte) (int, error) {
-	if len(p) == 0 {
-		return 0, nil
-	}
-
-	var n int
-	if sr.doReplace {
-		for {
-			rn, err := sr.replace.Read(p[n:])
-			n += rn
-
-			if err == io.EOF {
-				break
-				sr.doReplace = false
-			}
-
-			if n == len(p) {
-				return n, nil
-			}
-		}
-
-	}
-
-	// If the next n bytes are exactly the match condition
-	peek, err := sr.From.Peek(len(sr.What))
-	if err != nil && err != io.EOF {
-		return n, err
-	}
-
-	// Read until a the first known character
-	idx := bytes.IndexByte(peek, sr.What[0])
-
-	// Read position is exactly at the text we want to replace
-	if idx == 0 && bytes.Equal(sr.What, peek) {
-		sr.replace.Reset()
-		sr.From.Discard(len(sr.What))
-
-		// Attempt to copy what we can inside this call
-		diff := len(p) - n
-		if diff > 0 {
-			rn := copy(p[n:], sr.With)
-			n += rn
-		}
-
-		// Diff happens to be exactly or more the length of the replacement string
-		if diff >= len(sr.With) {
-			rn, err := sr.Read(p[n:])
-			return n + rn, err
-		}
-
-		// Write the remaining data later
-		sr.replace.Write(sr.With[diff:])
-		sr.doReplace = true
-		rn, err := sr.Read(p[n:])
-		return n + rn, err
-	}
-
-	if idx > 0 {
-		// Read until the correct index
-		rn, err := sr.From.Read(p[n:min(len(p), idx)])
-		return n + rn, err
-	}
-
-	// Nothing found
-	rn, err := sr.From.Read(p[n:min(len(p), len(sr.What))])
-	return n + rn, err
-}
-
-func (sr *StreamReplacer) Close() error {
-	return tap.Close(sr.From)
 }
 
 type ReplacePipe struct {
@@ -154,7 +53,7 @@ func (p *ReplacePipe) Go(ctx context.Context, stream pipe.Stream) error {
 		}
 
 		replacer := rewrite.New(r, []byte(wf), []byte(wd))
-		err = stream.Write(replacer)
+		err = stream.Write(tap.ReadProxyCloser(replacer, r))
 		if err != nil {
 			return err
 		}
